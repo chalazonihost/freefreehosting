d903258b41c39ccbf11500dc335d8fa33e7c1854
chalazonihost <chalazonihost@users.noreply.github.com>
2017-06-02 23:43:38 +0300

    Add files via upload

---
+++ hrkodikss.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<textarea rows="3" cols="35"textarea style="color:white; background-color: black">
+
+ <div style="background-color:WHITE;color:white;border:1px solid black;width:317px;height:78px;font-size:12px;">           <img src=" https://raw.githubusercontent.com/chalazonihost/freefreehosting/master/FM34.JPG " /><p>
+</p>
+    <audio id="audio-player" src="http://88.198.10.229:8026/;" controls="controls"autoplay="true"></audio>
+forma
+<form>
+
+</textarea>
+----------------------------------------------
+
